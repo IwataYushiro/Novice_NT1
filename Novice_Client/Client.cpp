0bca1b58faa992b2e621d835b2235b37f27e95d9
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-12-08 14:30:38 +0900

    当たり判定はこれでよし

--- Novice_Client/Client.cpp
+++ Novice_Client/Client.cpp
@@ -1,8 +1,12 @@
-﻿#include <Novice.h>
+﻿#include "Sprite.h"
+#include "TextureManager.h"
+#include "DirectXCommon.h"
+#include <Novice.h>
 #include <fstream>
 #include <math.h>
 #include <mmsystem.h>
 #include <process.h>
+
 #pragma comment(lib, "wsock32.lib")
 #pragma comment(lib, "winmm.lib")
 
@@ -13,10 +17,10 @@ const char kWindowTitle[] = "KAMATA ENGINEクライアント";
 typedef struct {
 	float x;
 	float y;
-} Vector2;
+} Vec2;
 
 typedef struct {
-	Vector2 center;
+	Vec2 center;
 	float radius;
 } Circle;
 
@@ -24,11 +28,21 @@ Circle a, b;
 Circle bulletA, bulletB;
 bool isShot = false;
 
-Vector2 center = {100, 100};
+Vec2 center = {100, 100};
 char keys[256] = {0};
 char preKeys[256] = {0};
-int color1 = RED;
-int color2 = BLUE;
+int colorA = RED;
+int colorB = BLUE;
+
+int lifeA = 3;
+int lifeB = 3;
+
+enum class Scene {
+
+	S_Title,
+	S_Game
+};
+Scene scene = Scene::S_Title;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -42,13 +56,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	hwMain = GetDesktopWindow();
 
-	a.center.x = 200;
-	a.center.y = 400;
-	a.radius = 50;
+	a.center.x = 200.0f;
+	a.center.y = 400.0f;
+	a.radius = 50.0f;
 
-	b.center.x = 800;
-	b.center.y = 400;
-	b.radius = 50;
+	b.center.x = 800.0f;
+	b.center.y = 400.0f;
+	b.radius = 50.0f;
+
+	uint32_t title = 1;
+	title = TextureManager::Load("title.png");
+
+	Sprite* sTitle = nullptr;
+	sTitle = Sprite::Create(title, {100.0f, 100.0f});
 
 	// winsock初期化
 	WSAStartup(MAKEWORD(2, 0), &wdData);
@@ -66,56 +86,77 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		if (keys[DIK_UP] != 0 || keys[DIK_W] != 0) {
-			a.center.y -= 5;
-		} else if (keys[DIK_DOWN] != 0 || keys[DIK_S] != 0) {
-			a.center.y += 5;
-		} else if (keys[DIK_RIGHT] != 0 || keys[DIK_D] != 0) {
-			a.center.x += 5;
-		} else if (keys[DIK_LEFT] != 0 || keys[DIK_A] != 0) {
-			a.center.x -= 5;
-		}
-
-		if (keys[DIK_SPACE] != 0) {
-			isShot = true;
-		}
-
-		if (isShot) {
-			if (bulletA.center.x >= 1000) {
-				isShot = false;
+		switch (scene) {
+		case Scene::S_Title:
+			if (keys[DIK_SPACE] != 0) {
+				scene = Scene::S_Game;
 			}
+
+			break;
+		case Scene::S_Game:
+			if (keys[DIK_UP] != 0 || keys[DIK_W] != 0) {
+				a.center.y -= 5.0f;
+			} else if (keys[DIK_DOWN] != 0 || keys[DIK_S] != 0) {
+				a.center.y += 5.0f;
+			} else if (keys[DIK_RIGHT] != 0 || keys[DIK_D] != 0) {
+				a.center.x += 5.0f;
+			} else if (keys[DIK_LEFT] != 0 || keys[DIK_A] != 0) {
+				a.center.x -= 5.0f;
+			}
+
+			if (keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] == 0) {
+				isShot = true;
+			}
+
+			if (isShot) {
+				if (bulletA.center.x >= 1000.0f) {
+					isShot = false;
+				}
+			}
+			break;
+		default:
+			break;
 		}
+
 		///
 		/// ↓更新処理ここから
 		///
 		// 弾の座標を同期
-		
 
 		if (!isShot) {
-			bulletA.center.x = a.center.x + 100;
+			bulletA.center.x = a.center.x + 100.0f;
 			bulletA.center.y = a.center.y;
-			bulletA.radius = 20;
-			
+			bulletA.radius = 20.0f;
+
 		} else {
-			const int speed = 10;
+			const float speed = 10.0f;
 			bulletA.center.x += speed;
 		}
-		bulletB.center.x = b.center.x - 100;
-			bulletB.center.y = b.center.y;
-			bulletB.radius = 20;
+		bulletB.center.x = b.center.x - 100.0f;
+		bulletB.center.y = b.center.y;
+		bulletB.radius = 20.0f;
 
 		float distanceA = sqrtf(
-		      (float)pow((double)a.center.x - (double)bulletB.center.x, 2) +
-		      (float)pow((double)a.center.y - (double)bulletB.center.y, 2));
+		  (float)pow((double)a.center.x - (double)bulletB.center.x, 2) +
+		  (float)pow((double)a.center.y - (double)bulletB.center.y, 2));
 
-		    float distanceB = sqrtf(
-		      (float)pow((double)b.center.x - (double)bulletA.center.x, 2) +
-		      (float)pow((double)b.center.y - (double)bulletA.center.y, 2));
+		float distanceB = sqrtf(
+		  (float)pow((double)b.center.x - (double)bulletA.center.x, 2) +
+		  (float)pow((double)b.center.y - (double)bulletA.center.y, 2));
 
-		    if (distanceB <= b.radius + bulletA.radius) {
-			color2 = RED;
-		    } else
-			color2 = BLUE;
+		if (distanceB <= b.radius + bulletA.radius) {
+			colorB = RED;
+			if (lifeB > 0) {
+
+				isShot = false;
+				lifeB--;
+			}
+		}
+		else {
+
+			colorB = BLUE;
+		}
+			
 		///
 		/// ↑更新処理ここまで
 		///
@@ -123,19 +164,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		    Novice::DrawEllipse(
-		      (int)a.center.x, (int)a.center.y, (int)a.radius, (int)a.radius, 0.0f, color1,
-		      kFillModeSolid);
-		    Novice::DrawEllipse(
-		      (int)b.center.x, (int)b.center.y, (int)b.radius, (int)b.radius, 0.0f, color2,
-		      kFillModeSolid);
+		// コマンドリストの取得
+		ID3D12GraphicsCommandList* commandList = DirectXCommon::GetInstance()->GetCommandList();
 
-		    Novice::DrawEllipse(
-		      (int)bulletA.center.x, (int)bulletA.center.y, (int)bulletA.radius,
-		      (int)bulletA.radius, 0.0f, WHITE, kFillModeSolid);
-		    Novice::DrawEllipse(
-		      (int)bulletB.center.x, (int)bulletB.center.y, (int)bulletB.radius,
-		      (int)bulletB.radius, 0.0f, WHITE, kFillModeSolid);
+		switch (scene) {
+		case Scene::S_Title:
+			// 前景スプライト描画前処理
+			Sprite::PreDraw(commandList);
+
+			/// <summary>
+			/// ここに前景スプライトの描画処理を追加できる
+			/// </summary>
+			sTitle->Draw();
+			//
+			// スプライト描画後処理
+			Sprite::PostDraw();
+			break;
+
+		case Scene::S_Game:
+			break;
+		}
+		Novice::DrawEllipse(
+		  (int)a.center.x, (int)a.center.y, (int)a.radius, (int)a.radius, 0.0f, colorA,
+		  kFillModeSolid);
+		Novice::DrawEllipse(
+		  (int)bulletA.center.x, (int)bulletA.center.y, (int)bulletA.radius, (int)bulletA.radius,
+		  0.0f, WHITE, kFillModeSolid);
+
+		Novice::DrawEllipse(
+		  (int)b.center.x, (int)b.center.y, (int)b.radius, (int)b.radius, 0.0f, colorB,
+		  kFillModeSolid);
+		Novice::DrawEllipse(
+		  (int)bulletB.center.x, (int)bulletB.center.y, (int)bulletB.radius, (int)bulletB.radius,
+		  0.0f, WHITE, kFillModeSolid);
 
 		///
 		/// ↑描画処理ここまで
@@ -152,6 +213,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの終了
 	Novice::Finalize();
+	delete sTitle;
 
 	// winsock終了
 	WSACleanup();
@@ -190,23 +252,30 @@ DWORD WINAPI Threadfunc(void* px) {
 		// データ送信
 		send(sConnect, (const char*)&a, sizeof(Circle), 0);
 		send(sConnect, (const char*)&bulletA, sizeof(Circle), 0);
-		send(sConnect, (const char*)&color2, sizeof(int), 0);
+		send(sConnect, (const char*)&colorB, sizeof(int), 0);
+		send(sConnect, (const char*)&scene, sizeof(Scene), 0);
+		send(sConnect, (const char*)&lifeB, sizeof(int), 0);
 
 		// データ受信
 		int nRcv = recv(sConnect, (char*)&b, sizeof(Circle), 0);
 		int nRcvB = recv(sConnect, (char*)&bulletB, sizeof(Circle), 0);
-		int nRcvCol1 = recv(sConnect, (char*)&color1, sizeof(int), 0);
-
+		int nRcvCol1 = recv(sConnect, (char*)&colorA, sizeof(int), 0);
+		int nRcvScene = recv(sConnect, (char*)&scene, sizeof(Scene), 0);
+		int nRcvLifeA = recv(sConnect, (char*)&lifeA, sizeof(Scene), 0);
+		
 		if (nRcv == SOCKET_ERROR)
 			break;
 		if (nRcvB == SOCKET_ERROR)
 			break;
 		if (nRcvCol1 == SOCKET_ERROR)
 			break;
+		if (nRcvScene == SOCKET_ERROR)
+			break;
+		if (nRcvLifeA == SOCKET_ERROR)
+			break;
 	}
 
 	shutdown(sConnect, 2);
 	closesocket(sConnect);
-
 	return 0;
 }
--- Novice_Server/Server.cpp
+++ Novice_Server/Server.cpp
@@ -2,6 +2,9 @@
 #include <math.h>
 #include <mmsystem.h>
 #include <process.h>
+#include "TextureManager.h"
+#include "Sprite.h"
+#include "DirectXCommon.h"
 
 #pragma comment(lib, "wsock32.lib")
 #pragma comment(lib, "winmm.lib")
@@ -13,10 +16,10 @@ const char kWindowTitle[] = "KAMATA ENGINEサーバ";
 typedef struct {
 	float x;
 	float y;
-} Vector2;
+} Vec2;
 
 typedef struct {
-	Vector2 center;
+	Vec2 center;
 	float radius;
 } Circle;
 
@@ -24,11 +27,21 @@ Circle a, b;
 Circle bulletA, bulletB;
 bool isShot = false;
 
-Vector2 center = {100, 100};
+Vec2 center = {100, 100};
 char keys[256] = {0};
 char preKeys[256] = {0};
-int color1 = RED;
-int color2 = BLUE;
+int colorA = RED;
+int colorB = BLUE;
+
+int lifeA = 3;
+int lifeB = 3;
+
+enum class Scene {
+
+	S_Title,
+	S_Game
+};
+Scene scene = Scene::S_Title;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -42,13 +55,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	hwMain = GetDesktopWindow();
 
-	a.center.x = 200;
-	a.center.y = 400;
-	a.radius = 50;
+	a.center.x = 200.0f;
+	a.center.y = 400.0f;
+	a.radius = 50.0f;
 
-	b.center.x = 800;
-	b.center.y = 400;
-	b.radius = 50;
+	b.center.x = 800.0f;
+	b.center.y = 400.0f;
+	b.radius = 50.0f;
+
+	uint32_t title = 1;
+	title = TextureManager::Load("title.png");
+
+	Sprite* sTitle = nullptr;
+	sTitle = Sprite::Create(title, {100.0f, 100.0f});
 
 	// winsock初期化
 	WSAStartup(MAKEWORD(2, 0), &wdData);
@@ -66,17 +85,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		if (keys[DIK_UP] != 0 || keys[DIK_W] != 0) {
-			b.center.y -= 5;
+		switch (scene) {
+		case Scene::S_Title:
+			if (keys[DIK_SPACE] != 0) {
+				scene = Scene::S_Game;
+			}
+
+			break;
+		case Scene::S_Game:
+			if (keys[DIK_UP] != 0 || keys[DIK_W] != 0) {
+			b.center.y -= 5.0f;
 		} else if (keys[DIK_DOWN] != 0 || keys[DIK_S] != 0) {
-			b.center.y += 5;
+			b.center.y += 5.0f;
 		} else if (keys[DIK_RIGHT] != 0 || keys[DIK_D] != 0) {
-			b.center.x += 5;
+			b.center.x += 5.0f;
 		} else if (keys[DIK_LEFT] != 0 || keys[DIK_A] != 0) {
-			b.center.x -= 5;
+			b.center.x -= 5.0f;
 		}
 
-		if (keys[DIK_SPACE] != 0 ) {
+		if (keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] == 0) {
 			isShot = true;
 		}
 
@@ -86,22 +113,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				isShot = false;
 			}
 		}
+			break;
+		default:
+			break;
+		}
+		
 
 		///
 		/// ↓更新処理ここから
 		///
 		//弾の座標を同期
-		bulletA.center.x = a.center.x + 100;
+		bulletA.center.x = a.center.x + 100.0f;
 		bulletA.center.y = a.center.y;
-		bulletA.radius = 20;
+		bulletA.radius = 20.0f;
 
 		if (!isShot) {
-			bulletB.center.x = b.center.x - 100;
+			bulletB.center.x = b.center.x - 100.0f;
 			bulletB.center.y = b.center.y;
-			bulletB.radius = 20;
+			bulletB.radius = 20.0f;
 		} 
 		else {
-			const int speed = 10;
+			const float speed = 10.0f;
 			bulletB.center.x -= speed;
 		}
 		
@@ -115,9 +147,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		  (float)pow((double)b.center.y - (double)bulletA.center.y, 2));
 
 		if (distanceA <= a.radius + bulletB.radius) {
-			color1 = BLUE;
+			colorA = BLUE;
+			if (lifeA <= 0) {
+
+			isShot = false;
+			lifeA--;
+			}
 		} 
-		else color1 = RED;
+		else {
+			
+			colorA = RED;
+		}
 		///
 		/// ↑更新処理ここまで
 		///
@@ -125,19 +165,43 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		Novice::DrawEllipse(
-		  (int)a.center.x, (int)a.center.y, (int)a.radius, (int)a.radius, 0.0f, color1,
+		// コマンドリストの取得
+		ID3D12GraphicsCommandList* commandList = DirectXCommon::GetInstance()->GetCommandList();
+
+		switch (scene) {
+		case Scene::S_Title:
+			// 前景スプライト描画前処理
+			Sprite::PreDraw(commandList);
+
+			/// <summary>
+			/// ここに前景スプライトの描画処理を追加できる
+			/// </summary>
+			sTitle->Draw();
+			//
+			// スプライト描画後処理
+			Sprite::PostDraw();
+			
+			break;
+
+		case Scene::S_Game:
+			break;
+
+		}
+
+			Novice::DrawEllipse(
+		  (int)a.center.x, (int)a.center.y, (int)a.radius, (int)a.radius, 0.0f, colorA,
 		  kFillModeSolid);
 		Novice::DrawEllipse(
-		  (int)b.center.x, (int)b.center.y, (int)b.radius, (int)b.radius, 0.0f, color2,
+		  (int)b.center.x, (int)b.center.y, (int)b.radius, (int)b.radius, 0.0f, colorB,
 		  kFillModeSolid);
-		
+
 		Novice::DrawEllipse(
-		  (int)bulletA.center.x, (int)bulletA.center.y, (int)bulletA.radius, (int)bulletA.radius, 0.0f, WHITE,
-		  kFillModeSolid);
+		  (int)bulletA.center.x, (int)bulletA.center.y, (int)bulletA.radius, (int)bulletA.radius,
+		  0.0f, WHITE, kFillModeSolid);
 		Novice::DrawEllipse(
-		  (int)bulletB.center.x, (int)bulletB.center.y, (int)bulletB.radius, (int)bulletB.radius, 0.0f, WHITE,
-		  kFillModeSolid);
+		  (int)bulletB.center.x, (int)bulletB.center.y, (int)bulletB.radius, (int)bulletB.radius,
+		  0.0f, WHITE, kFillModeSolid);
+
 		///
 		/// ↑描画処理ここまで
 		///
@@ -153,6 +217,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの終了
 	Novice::Finalize();
+	delete sTitle;
 
 	// winsock終了
 	WSACleanup();
@@ -211,20 +276,28 @@ DWORD WINAPI Threadfunc(void* px) {
 	while (1) {
 		// データ受信
 		int nRcv = recv(sConnect, (char*)&a, sizeof(Circle), 0);
-		int nRcvB = recv(sConnect, (char*)&bulletA, sizeof(Circle), 0);
-		int nRcvCol2 = recv(sConnect, (char*)&color2, sizeof(int), 0);
+		int nRcvA = recv(sConnect, (char*)&bulletA, sizeof(Circle), 0);
+		int nRcvColB = recv(sConnect, (char*)&colorB, sizeof(int), 0);
+		int nRcvScene = recv(sConnect, (char*)&scene, sizeof(Scene), 0);
+		int nRcvLifeB = recv(sConnect, (char*)&lifeB, sizeof(Scene), 0);
 		
 		if (nRcv == SOCKET_ERROR)
 			break;
-		if (nRcvB == SOCKET_ERROR)
+		if (nRcvA == SOCKET_ERROR)
 			break;
-		if (nRcvCol2 == SOCKET_ERROR)
+		if (nRcvColB == SOCKET_ERROR)
+			break;
+		if (nRcvScene == SOCKET_ERROR)
+			break;
+		if (nRcvLifeB == SOCKET_ERROR)
 			break;
 
 		// データ送信
 		send(sConnect, (const char*)&b, sizeof(Circle), 0);
 		send(sConnect, (const char*)&bulletB, sizeof(Circle), 0);
-		send(sConnect, (const char*)&color1, sizeof(int), 0);
+		send(sConnect, (const char*)&colorA, sizeof(int), 0);
+		send(sConnect, (const char*)&scene, sizeof(Scene), 0);
+		send(sConnect, (const char*)&lifeA, sizeof(int), 0);
 	}
 
 	shutdown(sConnect, 2);
